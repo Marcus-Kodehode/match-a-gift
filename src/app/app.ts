1f812ced679fb110afb635be8f6de26328c92145
Marcus <marcus29sfj@gmail.com>
2025-09-24 11:20:41 +0200

    500 took a sick day

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <!-- Fullscreen, fixed bilde-background -->
-    <div class="bg-img">
-      <img class="bg-img__el" src="/images/BG8.webp" alt="Background" />
-      <div class="bg-img__overlay"></div>
-    </div>
-
-    <!-- App UI -->
-    <div class="app-shell min-h-screen">
-      <router-outlet></router-outlet>
-    </div>
-  `,
+  templateUrl: './app.html',
 })
 export class App {}
